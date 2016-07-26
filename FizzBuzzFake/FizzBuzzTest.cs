aeef21567d354eb4f5b9f267eef3e3a95508b662
Llewellyn Falco <isidore@setgame.com>
2016-07-26 07:04:12 -0700

    Failling Test

--- FizzBuzzFake/FizzBuzzTest.cs
+++ FizzBuzzFake/FizzBuzzTest.cs
@@ -4,11 +4,13 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace FizzBuzzFake
 {
     [TestClass]
-    public class UnitTest1
+    public class FizzBuzzTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestFizzBuzz()
         {
+            string expected = "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,18,19,Buzz";
+            Assert.AreEqual(expected. FizzBuzz.print(20));
         }
     }
 }
