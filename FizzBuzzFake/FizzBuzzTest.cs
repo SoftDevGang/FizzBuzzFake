caeb06ff0fa7f3a1508e96947fdc9277ec9079ac
Llewellyn Falco <isidore@setgame.com>
2016-07-26 07:24:34 -0700

    concat

--- FizzBuzzFake/FizzBuzzTest.cs
+++ FizzBuzzFake/FizzBuzzTest.cs
@@ -21,7 +21,7 @@ namespace FizzBuzzFake
             string first ="";
             for (int i = 1; i < 2; i++)
             {
-                first = Convert(1);
+                first += Convert(1);
             
             }
             return string.Format("{0},2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,18,19,Buzz", first);
