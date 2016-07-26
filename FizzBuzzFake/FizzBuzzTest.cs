756ec57ab1d53730c6ae6d19c2a5e718f6c0faf7
Llewellyn Falco <isidore@setgame.com>
2016-07-26 07:06:37 -0700

    Passing Test

--- FizzBuzzFake/FizzBuzzTest.cs
+++ FizzBuzzFake/FizzBuzzTest.cs
@@ -10,7 +10,16 @@ namespace FizzBuzzFake
         public void TestFizzBuzz()
         {
             string expected = "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,18,19,Buzz";
-            Assert.AreEqual(expected. FizzBuzz.print(20));
+            Assert.AreEqual(expected, FizzBuzz.Print(20));
+        }
+    }
+
+    public class FizzBuzz
+    {
+        public static string Print(int top)
+        {
+            return "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,18,19,Buzz";
+            
         }
     }
 }
