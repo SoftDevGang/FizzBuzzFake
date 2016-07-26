7883c1d91b5daeeba18aba4fbef9ca818d8702e1
Llewellyn Falco <isidore@setgame.com>
2016-07-26 07:22:06 -0700

    loop

--- FizzBuzzFake/FizzBuzzTest.cs
+++ FizzBuzzFake/FizzBuzzTest.cs
@@ -18,7 +18,12 @@ namespace FizzBuzzFake
     {
         public static string Print(int top)
         {
-            var first = Convert(1);
+            string first ="";
+            for (int i = 1; i < 2; i++)
+            {
+                first = Convert(1);
+            
+            }
             return string.Format("{0},2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,18,19,Buzz", first);
         }
 
