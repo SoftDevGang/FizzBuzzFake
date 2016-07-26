7bed71a7b34c762b10ea16decaa448f140bd596e
Llewellyn Falco <isidore@setgame.com>
2016-07-26 07:08:07 -0700

    extracted 1

--- FizzBuzzFake/FizzBuzzTest.cs
+++ FizzBuzzFake/FizzBuzzTest.cs
@@ -18,8 +18,8 @@ namespace FizzBuzzFake
     {
         public static string Print(int top)
         {
-            return "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,18,19,Buzz";
-            
+            var first = "1";
+            return string.Format("{0},2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,18,19,Buzz", first);
         }
     }
 }
